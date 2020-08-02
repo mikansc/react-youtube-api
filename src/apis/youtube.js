5077d988b9c451230ef1d4233556345b60162cc9
Michael Nascimento <michael.nsc@outlook.com>
2020-08-02 03:43:24 -0300

    Bug fixes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'YOUR GOOGLE API KEY';
+const KEY = process.env.GAPI_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
